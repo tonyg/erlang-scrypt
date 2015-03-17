905416f49ff16e7bdad43941e9099894406278a7
Bikram Chatterjee <razorpeak@gmail.com>
2015-03-17 23:49:25 +0100

    code cleanup

--- src/scrypt_nif.erl
+++ src/scrypt_nif.erl
@@ -3,7 +3,7 @@
 -on_load(init/0).
 
 init() ->
-    ok = erlang:load_nif(erlscrypt:priv_dir() ++ "/scrypt_nif", 0).
+    erlang:load_nif(erlscrypt:priv_dir() ++ "/scrypt_nif", 0).
 
 scrypt(_Passwd, _Salt, _N, _R, _P, _Buflen) ->
     exit(nif_library_not_loaded).
